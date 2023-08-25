e6b06a3041b665c0f0f6e48f1c0c977059956f70
andrew <anntp@twendeesoft.com>
2023-08-25 10:48:19 +0700

    1: Fix migration file and dependencies

--- src/_modules_/auth/auth.dto.ts
+++ src/_modules_/auth/auth.dto.ts
@@ -1,21 +1,42 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { UserRole } from "@prisma/client";
 import { IsEmail, IsEnum, IsNotEmpty, IsStrongPassword } from "class-validator";
 
 export class SignUpDto {
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @IsStrongPassword()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    enum: UserRole,
+    description: 'This is a required property',
+  })
   @IsEnum(UserRole)
   role: UserRole;
 }
