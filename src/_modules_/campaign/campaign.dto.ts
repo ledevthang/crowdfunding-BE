c8c06345f19f989f8666d4888fea64d5c7acd7aa
andrew <anntp@twendeesoft.com>
2023-08-30 14:30:27 +0700

    28: api get one, create campaign and get all categories

--- src/_modules_/campaign/campaign.dto.ts
+++ src/_modules_/campaign/campaign.dto.ts
@@ -1,6 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Campaign } from '@prisma/client';
+import { IsDateString, IsNotEmpty, IsNumber } from 'class-validator';
 import { BasePagingDto, BasePagingResponse } from 'utils/base.dto';
 
 export class FindCampaignDto extends BasePagingDto {}
 
 export class FindCampaignsResultDto extends BasePagingResponse<Campaign> {}
+
+export class CreateCampaignDto {
+  @ApiProperty({
+    description: 'This is a required property'
+  })
+  @IsNotEmpty()
+  title: string;
+
+  description: string;
+
+  @ApiProperty({
+    description: 'This is a required property'
+  })
+  @IsNotEmpty()
+  localtion: string;
+
+  @ApiProperty({
+    description: 'This is a required property'
+  })
+  @IsDateString()
+  startAt: Date;
+
+  @ApiProperty({
+    description: 'This is a required property'
+  })
+  @IsDateString()
+  endAt: Date;
+
+  @ApiProperty({
+    description: 'This is a required property'
+  })
+  @IsNumber()
+  goal: number;
+
+  campaignTags: string[];
+
+  categoryIds: number[];
+}
--- src/_modules_/campaign/campaign.service.ts
+++ src/_modules_/campaign/campaign.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '_modules_/prisma/prisma.service';
-import { FindCampaignDto, FindCampaignsResultDto } from './campaign.dto';
+import {
+  CreateCampaignDto,
+  FindCampaignDto,
+  FindCampaignsResultDto
+} from './campaign.dto';
+import { Campaign } from '@prisma/client';
 
 @Injectable()
 export class CampaignService {
@@ -16,15 +21,90 @@ export class CampaignService {
       this.prisma.campaign.findMany({
         take: size,
         skip: skip,
+        include: {
+          categories: {
+            select: {
+              category: {
+                select: {
+                  id: true,
+                  name: true
+                }
+              }
+            }
+          }
+        }
       }),
       await this.prisma.campaign.count()
     ]);
+    const result = campaigns.map(campaign => {
+      return {
+        ...campaign,
+        categories: campaign.categories.map(category => category.category)
+      };
+    });
     return {
-      data: campaigns,
+      data: result,
       page: page,
       size: size,
       totalPages: Math.ceil(count / size) || 0,
       totalElement: count
     };
+  }
+
+  async findOne(id: number): Promise<Campaign> {
+    console.log(id);
+    const campaign = await this.prisma.campaign.findUnique({
+      where: { id },
+      include: {
+        categories: {
+          select: {
+            category: {
+              select: {
+                id: true,
+                name: true
+              }
+            }
+          }
+        }
+      }
+    });
+    const result = {
+      ...campaign,
+      categories: campaign.categories.map(category => category.category)
+    };
+    return result;
+  }
+
+  async create(creatorId: number, createCampaignDto: CreateCampaignDto) {
+    const {
+      title,
+      description,
+      localtion,
+      startAt,
+      endAt,
+      goal,
+      campaignTags,
+      categoryIds
+    } = createCampaignDto;
+    try {
+      await this.prisma.campaign.create({
+        data: {
+          title,
+          description,
+          localtion,
+          startAt,
+          endAt,
+          goal,
+          campaignTags,
+          creatorId,
+          categories: {
+            create: categoryIds.map(id => ({ categoryId: id }))
+          }
+        }
+      });
+      return { message: 'success' };
+    } catch (error) {
+      return { message: 'fail!' };
+    }
   }
 }
