c861c86ed773bdbfcfa1b2c3e9943f840be8ca18
andrew <anntp@twendeesoft.com>
2023-09-07 16:42:37 +0700

    Campaign & Transaction: Fix decorator

--- src/_modules_/campaign/campaign.dto.ts
+++ src/_modules_/campaign/campaign.dto.ts
@@ -59,59 +59,50 @@ export class CreateCampaignDto {
   categoryIds: number[];
 }
 export class FindCampaignDto extends BasePagingDto {
-  @ApiProperty({
+  @OptionalProperty({
     type: 'string',
     required: false,
     description: 'Sort field and order. ex: title,asc'
   })
-  @OptionalProperty()
   @Transform(param => param.value.split(','))
   sort?: string[] = ['id', 'asc'];
 
-  @ApiProperty({ required: false, description: 'Search query' })
-  @OptionalProperty()
+  @OptionalProperty({ description: 'Search query' })
   query?: string = '';
 
-  @ApiProperty({
+  @OptionalProperty({
     type: 'string',
     required: false,
     description: 'Category Ids. ex: 1,2,3'
   })
-  @OptionalProperty()
   @Transform(param => param.value.split(',').map(i => Number(i)))
   categoryIds?: number[];
 }
 
 export class FindCampaignsResultDto extends BasePagingResponse<Campaign> {}
 export class FindFundedCampaignDto extends BasePagingDto {
-  @ApiProperty({ required: false })
   @IsFloat
   @OptionalProperty()
   minAmount?: number;
 
-  @ApiProperty({ required: false })
   @IsFloat
   @OptionalProperty()
   maxAmount?: number;
 
-  @ApiProperty({ required: false })
   @OptionalProperty()
   campaignTitle?: string;
 
-  @ApiProperty({ required: false })
   @OptionalProperty()
   startDate?: Date;
 
-  @ApiProperty({ required: false })
   @OptionalProperty()
   endDate?: Date;
 
-  @ApiProperty({
+  @OptionalProperty({
     type: 'string',
     required: false,
     description: 'Between 3 stauses: ON_GOING, FAILED, SUCCEED.'
   })
-  @OptionalProperty()
   @Transform(param => param.value.split(','))
   states?: FundCampaignStatus[];
 }
--- src/_modules_/transaction/transaction.dto.ts
+++ src/_modules_/transaction/transaction.dto.ts
@@ -20,40 +20,34 @@ export class CreateTransactionDto {
 }
 
 export class FindTransactionDto extends BasePagingDto {
-  @ApiProperty({ required: false })
   @OptionalProperty()
   @Transform(param => Number(param.value) || null)
   campaignId?: number;
 
-  @ApiProperty({ required: false })
   @IsFloat
   @OptionalProperty()
   minAmount?: number;
 
-  @ApiProperty({ required: false })
   @IsFloat
   @OptionalProperty()
   maxAmount?: number;
 
-  @ApiProperty({ required: false })
   @OptionalProperty()
   campaignTitle?: string;
 
-  @ApiProperty({ required: false })
   @OptionalProperty()
   startDate?: Date;
 
-  @ApiProperty({ required: false })
   @OptionalProperty()
   endDate?: Date;
 
-  @ApiProperty({
+  @Transform(param => param.value.split(','))
+  @OptionalProperty({
     type: 'string',
     required: false,
-    description: 'Between 3 stauses: PENDING, PROCESSED, REFUNDED. Ex: PENDING,PROCESSED'
+    description:
+      'Between 3 stauses: PENDING, PROCESSED, REFUNDED. Ex: PENDING,PROCESSED'
   })
-  @Transform(param => param.value.split(','))
-  @OptionalProperty()
   states?: TransactionStatus[];
 }
 
